3bc0a1399c313534d5f520001994f027b0aa6a61
Michael Cheung <35844248+MichaelCheung105@users.noreply.github.com>
2021-07-10 17:33:44 +0800

    update environment init

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from logzero import logger
 from config import HOME_PATH, config
 
 
@@ -9,16 +9,25 @@ class Runner():
 
     @staticmethod
     def read_data(data_source):
-        data_path = config['directories']['data_dir']
+        data_path = config['DEFAULT']['data_dir']
         data = np.load(f"{HOME_PATH}/{data_path}/{data_source}_data.npz")
         return data['f0'], data['f1']
 
     def run(self):
-        # train_x, train_y = self.read_data(data_source='train')
-        test_x, test_y = self.read_data(data_source='train')
-        print(test_y)
+        experiment_mode = config['DEFAULT']['experiment_mode']
+        logger.info(f'experiment mode: {experiment_mode}')
+
+        if 'train' in experiment_mode:
+            logger.info('loading train data')
+            train_x, train_y = self.read_data(data_source='train')
+
+        if 'test' in experiment_mode:
+            logger.info('loading test data')
+            test_x, test_y = self.read_data(data_source='train')
 
 
 if __name__ == '__main__':
+    logger.info('program begins')
     runner = Runner()
     runner.run()
+    logger.info('program ends')
