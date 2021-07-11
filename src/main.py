91dda03e45b10a296a2de3ab8287455f9e86737c
Michael Cheung <35844248+MichaelCheung105@users.noreply.github.com>
2021-07-11 14:20:16 +0800

    enable the saving of models

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import pickle
 from datetime import datetime
 
 import numpy as np
@@ -25,22 +26,35 @@ class Runner:
         return samples, labels
 
     def log_result(self, result_dict):
-        cols = ['category', 'logloss', 'accuracy', 'precision', 'recall', 'tp', 'fp', 'fn', 'tn', 'dataset', 'remarks']
-        merged_metrics_df = pd.concat(result_dict['merged_metrics_df']).assign(remarks=config.get('DEFAULT', 'remarks'))
+        cols = ['category', 'logloss', 'accuracy', 'precision', 'recall', 'tp', 'fp', 'fn', 'tn', 'dataset']
+        merged_metrics_df = pd.concat(result_dict['merged_metrics_df'])
         merged_metrics_df.columns = cols
-        merged_metrics_df.assign(experiment_id=self.create_dt)
-        # TODO: log result as csv
+        merged_metrics_df = merged_metrics_df.assign(experiment_id=self.create_dt,
+                                                     remarks=config.get('DEFAULT', 'remarks'))
+
+        # Print result
         pd.set_option('display.max_columns', 20)
         pd.set_option('display.width', 200)
         logger.info(f"\n{merged_metrics_df[merged_metrics_df.dataset == 'train']}")
         logger.info(f"\n{merged_metrics_df[merged_metrics_df.dataset == 'test']}")
+
+        # Log result as csv
+        accuracy_path = config.get('DEFAULT', 'accuracy_path')
+        save_path = f"{HOME_PATH}/{accuracy_path}/{self.create_dt}_merged_metrics_df.csv"
+        merged_metrics_df.to_csv(save_path)
+
+    def save_model(self):
+        model_path = config.get('DEFAULT', 'model_path')
+        save_path = f"{HOME_PATH}/{model_path}/{self.create_dt}_model.pkl"
+        with open(save_path, 'wb') as f:
+            pickle.dump(self.trainer.model.state_dict(), f)
 
     def run(self):
         experiment_mode = config.get('DEFAULT', 'experiment_mode')
         logger.info(f'experiment mode: {experiment_mode}')
 
         # Prepare a dictionary to log training results
-        result_dict = {'confusion_matrices': [],
+        result_dict = {'confusion_matrices': {'train': None, 'test': None},
                        'merged_metrics_df': []
                        }
 
@@ -53,18 +67,21 @@ class Runner:
                                                                                         predicted_class=predicted_class,
                                                                                         y_pred_prob=probability_per_class)
             result_dict['merged_metrics_df'].append(train_metrics_df.assign(dataset='train'))
+            result_dict['confusion_matrices']['train'] = train_conf_matrix
 
         if 'test' in experiment_mode:
             logger.info('loading test data')
-            test_x, test_y = self.read_data(data_source='train')
+            test_x, test_y = self.read_data(data_source='test')
             predicted_class, probability_per_class = self.trainer.inference(test_x)
             test_conf_matrix, test_metrics_df = self.metric_handler.calculate_metrics(labels=test_y,
                                                                                       predicted_class=predicted_class,
                                                                                       y_pred_prob=probability_per_class)
             result_dict['merged_metrics_df'].append(test_metrics_df.assign(dataset='test'))
+            result_dict['confusion_matrices']['test'] = test_conf_matrix
 
-        # Log result
+        # Log results & Save model
         self.log_result(result_dict)
+        self.save_model()
 
 
 if __name__ == '__main__':
