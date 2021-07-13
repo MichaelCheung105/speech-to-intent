9fa0ecf0096007dde6961ebffd082972f57dd430
Michael Cheung <lupus.cheung@gmail.com>
2021-07-13 13:20:40 +0800

    Add CnnLSTM model V2

--- src/models.py
+++ src/models.py
@@ -57,6 +57,44 @@ class CnnLSTM(nn.Module):
         return linear_output
 
 
+class CnnLSTMV2(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # Config
+        in_channels = config.getint('CnnLSTM', 'in_channels')
+        out_channels = config.getint('CnnLSTM', 'out_channels')
+        kernel_size_h = config.getint('CnnLSTM', 'kernel_size_h')
+        kernel_size_w = config.getint('CnnLSTM', 'kernel_size_w')
+        dilation_h = config.getint('CnnLSTM', 'dilation_h')
+        dilation_w = config.getint('CnnLSTM', 'dilation_w')
+        stride_h= config.getint('CnnLSTM', 'stride_h')
+        stride_w = config.getint('CnnLSTM', 'stride_w')
+        padding = config.get('CnnLSTM', 'padding')
+        input_size = config.getint('CnnLSTM', 'input_size')
+        hidden_layer_size = config.getint('CnnLSTM', 'hidden_layer_size')
+        output_size = config.getint('CnnLSTM', 'output_size')
+
+        # Network
+        self.cnn = nn.Conv2d(in_channels=in_channels, out_channels=out_channels,
+                             kernel_size=(kernel_size_h, kernel_size_w), dilation=(dilation_h, dilation_w),
+                             stride=(stride_h, stride_w), padding=padding)
+        self.cnn2 = nn.Conv2d(in_channels=16, out_channels=8,
+                             kernel_size=(3, 1), dilation=(2, 1),
+                             stride=(3, 1), padding=padding)
+        self.lstm = nn.LSTM(input_size=8, hidden_size=hidden_layer_size, batch_first=True)
+        self.linear = nn.Linear(hidden_layer_size, output_size)
+
+    def forward(self, input_seq):
+        input_seq = torch.unsqueeze(input_seq, 1)
+        cnn_output = self.cnn(input_seq)
+        cnn_output = self.cnn2(cnn_output)
+        cnn_output = torch.squeeze(cnn_output)
+        cnn_output = cnn_output.swapaxes(1, 2)
+        lstm_output, (hn, cn) = self.lstm(cnn_output)
+        linear_output = self.linear(lstm_output[:, -1, :])
+        return linear_output
+
+
 class CnnMaxPool(nn.Module):
     def __init__(self):
         super().__init__()
